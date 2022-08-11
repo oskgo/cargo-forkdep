bc38dbbb1964477c7ca3207f413e9197934632e6
bowlerman <oskar@goldhahn.info>
2022-08-11 03:13:49 +0200

    modify manifest

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use anyhow::{anyhow, Result};
 use cargo::{
-    core::{package_id, EitherManifest, Manifest, PackageSet, SourceId, SourceMap},
+    core::{package_id, EitherManifest, Manifest, PackageSet, SourceId, SourceMap, Dependency, Package, Workspace},
     ops::{generate_lockfile, load_pkg_lockfile},
     sources::{GitSource, RegistrySource},
     util::{config::Config, important_paths::find_root_manifest_for_wd, toml::TomlManifest},
 };
 use clap::Parser;
+use toml_edit::Item;
 use std::{
     collections::HashSet,
-    path::{Path, PathBuf},
+    path::{Path, PathBuf}, fs, str::FromStr,
 };
 
 #[derive(Parser, Debug)] // requires `derive` feature
@@ -34,29 +35,42 @@ fn main() -> Result<()> {
         .manifest_path
         .map(|v| Ok(v))
         .unwrap_or_else(|| find_root_manifest_for_wd(&std::env::current_dir()?))?;
-    let mut workspace = cargo::core::Workspace::new(&manifest_path, &config)?;
-    let config = workspace.config().to_owned();
-    let lockfile = match load_pkg_lockfile(&workspace)? {
+    let mut workspace = Workspace::new(&manifest_path, &config)?;
+    let repo = get_repo(&mut workspace, &args.dependency)?;
+    let mut manifest = dbg!(read_manifest(manifest_path)?);
+    manifest["patch"]["crates.io"][args.dependency]["git"] = <Item as FromStr>::from_str(&repo)?;
+    Ok(())
+}
+
+fn read_manifest(manifest_path: PathBuf) -> Result<toml_edit::Document> {
+    let data = fs::read_to_string(&manifest_path)?;
+    Ok(data.parse()?)
+}
+
+fn get_repo(workspace: &Workspace, dependency: &str) -> Result<String> {
+    let config = workspace.config();
+    let lockfile = match load_pkg_lockfile(&*workspace)? {
         Some(lockfile) => lockfile,
         None => {
-            generate_lockfile(&workspace)?;
-            load_pkg_lockfile(&workspace)?.ok_or_else(|| anyhow!("Failed to generate lockfile"))?
+            generate_lockfile(&*workspace)?;
+            load_pkg_lockfile(&*workspace)?.ok_or_else(|| anyhow!("Failed to generate lockfile"))?
         }
     };
-    let dep_repo: Option<String> = None;
-    for package in workspace.members_mut() {
+    for package in workspace.members() {
         let package_id = package.package_id();
-        for (dep, _) in lockfile
+        for (dep_id, _) in lockfile
             .deps(package_id)
-            .filter(|(id, _)| id.name().as_str() == &args.dependency)
+            .filter(|(id, _)| id.name().as_str() == dependency)
         {
             let mut sources = SourceMap::new();
-            sources.insert(dep.source_id().load(config, &HashSet::new())?);
-            let deps = [dep];
+            sources.insert(dep_id.source_id().load(config, &HashSet::new())?);
+            let deps = [dep_id];
             let pkg_set = PackageSet::new(&deps, sources, config)?;
-            let package = pkg_set.get_one(dep)?;
-            package.manifest().metadata().repository.as_ref().ok_or_else(|| anyhow!("No repository in manifest of {}", dep.name()))?;
+            let package = pkg_set.get_one(dep_id)?;
+            if let Some(repo) = &package.manifest().metadata().repository {
+                return Ok(repo.clone())
+            }
         }
     }
-    Ok(())
+    Err(anyhow!("Could not find use of dependency {}", dependency))
 }
